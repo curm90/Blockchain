e7fbeab8269621e943eae53c9389e88b05752faa
Liam Sutton <devliam90@gmail.com>
2020-03-11 16:05:32 +0000

    last block endpoint

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -52,6 +52,8 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
+    mined_coins = 0
+
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -65,7 +67,8 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        last_block = data['last_block']
+        new_proof = proof_of_work(last_block)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -76,4 +79,8 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data['message'] == 'New Block Forged':
+            mined_coins += 1
+            print(mined_coins)
+        else:
+            print(data['message'])
